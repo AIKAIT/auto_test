92857eec5577ab74acdf4d57c9693462107957f8
Artur Kaitmazov <kaitmazov@gmail.com>
2016-09-01 21:39:58 +0300

    Новая структура проекта, добавлены задачи для спрайтов, собрано меню под мобильные

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 //cd /c/Users/Artur/documents/work_hard/auto_test
 /*NODE_ENV=production gulp less  -  запускает сборку без SOURCEMAPS*/
+
 var gulp = require('gulp');
 var less = require('gulp-less');                                                //компилятор
 var debug = require('gulp-debug');                                              //отображает в консоли происходящее
@@ -24,6 +25,8 @@ var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 var csscomb = require('gulp-csscomb');
 var rigger = require('gulp-rigger');
+var spritesmith = require('gulp.spritesmith');
+var mediaCombine= require('gulp-group-css-media-queries');
 
 var imgSrc = 'src/img/*.*';
 var imgDst = 'build/img';
@@ -41,14 +44,10 @@ gulp.task('less', function() {
         };
       })
     }))
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))  /*для удаления SOURCEMAPS на продакшене*/
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))                             /*для удаления SOURCEMAPS на продакшене*/
     .pipe(debug({title: 'src-----------'}))
     .pipe(remember('less'))
     .pipe(less())
-    // .on('error', notify.onError({
-    //     message: "Ошибка: <%= error.message %>",
-    //     title: "Ошибка компиляции LESS"
-    // }))
     .pipe(debug({title: 'less----------'}))
     .pipe(prefixer( {
           browsers: ['last 2 versions'],
@@ -57,37 +56,48 @@ gulp.task('less', function() {
     .pipe(debug({title: 'autoprefixer--'}))
     .pipe(csscomb())
     .pipe(debug({title: 'csscomb-------'}))
-    .pipe(cssmin())
-    .pipe(debug({title: 'cssmin--------'}))
+    // .pipe(cssmin())                                                          // Отключено для удобочитаемости (включить при сборке)
+    // .pipe(debug({title: 'cssmin--------'}))                                  // Отключено для удобочитаемости (включить при сборке)
     .pipe(rename({ prefix: 'min.'}))
     .pipe(debug({title: 'rename--------'}))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write()))  /*для удаления SOURCEMAPS на продакшене*/
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))                            /*для удаления SOURCEMAPS на продакшене*/
     .pipe(size({title: '-CSS'}))
     .pipe(gulp.dest('build'))
     .pipe(debug({title: 'build---------'}));
     // .pipe(reload({stream: true}));
 });
 
+
 gulp.task('html', function() {
   console.log('-------------------------- Обработка HTML ---------------');
   return gulp.src('src/*.html')
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+          message: "Ошибка: <%= error.message %>",
+          title: "Ошибка компиляции HTML"
+        };
+      })
+    }))
     .pipe(rigger())
     .pipe(debug({title: 'rigger --------'}))
     .pipe(gulp.dest('build'));
+    // .pipe(reload({stream: true}));
 });
 
 
 gulp.task('img', function() {
   console.log('------------------------- Обработка IMG ----------------');
-  return gulp.src('src/img/**/*.*')
-    // .pipe(plumber({
-    //   errorHandler: notify.onError(function(err) {
-    //     return {
-    //       message: "Ошибка: <%= error.message %>",
-    //      title: "Ошибка обработки IMG"
-    //     };
-    //   })
-    // }))
+  //return gulp.src('src/img/*.{jpg,png}')                                        //JPG & PNG !ONLY!
+    return gulp.src('src/img/*.*')
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+          message: "Ошибка: <%= error.message %>",
+         title: "Ошибка обработки IMG"
+        };
+      })
+    }))
     .pipe(debug({title: 'src-----------'}))
     .pipe(newer(imgDst))
     .pipe(imagemin({
@@ -104,12 +114,37 @@ gulp.task('img', function() {
 });
 
 
+gulp.task('sprite', function() {                                                //Делает PNG спрайт
+  console.log('-------------------------- Делаю SPRITE -----------------');
+  return gulp.src('src/img/png_sprite/*.png')
+    .pipe(spritesmith ({
+      imgName: 'sprite__png.png',
+      cssFormat: 'less',
+      cssName: 'sprite.less',
+      algorithm: 'top-down',
+    }))
+    .pipe(debug({title: 'делаю ---------'}))
+    .pipe(gulp.dest('src/img'))
+    .pipe(size({title: '--- the size of'}));
+});
+
+
+gulp.task('cmq', function() {                                                   //Комбинирует media queries
+  // return gulp.src('/build/min.style.css')
+  gulp.src('build/style.css')
+    .pipe(debug({title: 'беру ---------'}))
+    .pipe(mediaCombine())
+    .pipe(debug({title: 'делаю ---------'}))
+    .pipe(gulp.dest('src/css'))
+    .pipe(size({title: '--- the size of'}));
+});
+
 
 gulp.task('del', function() {
     console.log('------------------------- Удаляю build/ ---------------');
     return del('build');
-
 });
+
 
 // gulp.task('del', function() {
 //   console.log('---------- Удаляю build/');
@@ -120,7 +155,7 @@ gulp.task('del', function() {
 // });
 
 
-gulp.task('build', ['del','html', 'less', 'img']);
+gulp.task('build', ['del', 'less','html', 'img']);
 
 
 gulp.task('watch', function() {
@@ -132,10 +167,13 @@ gulp.task('watch', function() {
 
 gulp.task('serve', function() {
   browserSync.init({
-    server: 'build',
-    tunnel: true
+    server: 'BUILD',
+    // tunnel: true
   });
-  browserSync.watch('build/**/*.*').on('change', browserSync.reload);
+  browserSync.watch('build/**/*.*').on('change', browserSync.reload).on('error', notify.onError({
+        message: "Ошибка: <%= error.message %>",
+        title: "Я не сделяль :("
+    }));
 });
 
 
@@ -154,3 +192,4 @@ gulp.task('serve', function() {
 //         }
 //     });
 // });
+
